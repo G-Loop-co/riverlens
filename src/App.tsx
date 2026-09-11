a12dc615d72b05bd517ef348273c2694a5995e9f
imycc <yc@drivelabai.com>
2026-09-11 11:16:45 +0800

    Display the v0.3.0 release version in navigation

--- src/App.tsx
+++ src/App.tsx
@@ -285,7 +285,7 @@ export default function App() {
           </div>
           <div className="version">
             <span>{t("RiverLens")}</span>
-            <span>v0.2.0</span>
+            <span>v0.3.0</span>
           </div>
         </div>
       </aside>
